57f6dc2cac3b0310450b0469f50a1a69b07ea914
guismeiram <guismeiram@gmail.com>
2023-01-29 18:20:52 -0300

    Domain

--- DESKTOP/ESConWin/src/ESCon-Domain/Entities/Banco.cs
+++ DESKTOP/ESConWin/src/ESCon-Domain/Entities/Banco.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ESCon_CrossCutting.BuildingBlocks.Entitys;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,16 @@ using System.Threading.Tasks;
 
 namespace ESCon_Domain.Entities
 {
-    internal class Banco
+    public class Banco : Entity
     {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public int Agencia { get; set; }
+        public int Conta_Banco { get; set; }
+        public int Gerente_Banco { get; set; }
+        public int Fone_Banco { get; set; }
+        public Pagamento Pagamento { get; set; }
+        public Recebimento Recebimento { get; set; }
+
     }
 }
--- DESKTOP/ESConWin/src/ESCon-Domain/Entities/Cartao.cs
+++ DESKTOP/ESConWin/src/ESCon-Domain/Entities/Cartao.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ESCon_CrossCutting.BuildingBlocks.Entitys;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,11 @@ using System.Threading.Tasks;
 
 namespace ESCon_Domain.Entities
 {
-    internal class Cartao
+    public class Cartao : Entity
     {
+        public int Codigo { get; set; }
+        public string Nome_Cartao { get; set; }
+        public double Taxa_Cartao { get; set; }
+        public Venda Venda { get; set; }
     }
 }
